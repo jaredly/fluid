b10289eb2d576e94e8010159eccb2d9d6d06bbae
Jared Forsyth <jared@jaredforsyth.com>
2019-02-14 19:26:50 -0700

    all the caching

--- src/native/fluid_column_browser.c
+++ src/native/fluid_column_browser.c
@@ -1,23 +1,63 @@
 #include "./fluid_shared.h"
 
+@interface MyPair: NSObject <NSCopying>
+  @property (assign) int child;
+  @property (strong) NSString *id;
+  - (NSUInteger)hash;
+@end
+
+@implementation MyPair
+- (id)copyWithZone:(NSZone *)zone {
+  MyPair *copy = [MyPair alloc];
+  copy.id = self.id;
+  copy.child = self.child;
+  return copy;
+}
+
+- (NSUInteger)hash
+{
+    return [self.id hash] ^ self.child;
+}
+
+- (BOOL)isEqual:(id)object {
+  if (object == nil) {
+    return NO;
+  }
+  MyPair *other = (MyPair *)object;
+  return other.child == self.child && [self.id isEqual:other.id];
+}
+@end
 
 @interface BrowserDelegate: NSObject <NSBrowserDelegate>
   @property (nonatomic) int childrenCount;
   @property (nonatomic) int childOfItem;
   @property (nonatomic) int isLeafItem;
   @property (nonatomic) int displayForItem;
+  - (void)clearCache;
 @end
 
 @implementation BrowserDelegate {
   NSMutableDictionary<NSString *, NSString *> *objects;
+  NSMutableDictionary<NSString *, NSString *> *displayCache;
+  NSMutableDictionary<NSString *, NSNumber *> *isLeafCache;
+  NSMutableDictionary<MyPair *, NSString *> *childCache;
 }
 
 - (instancetype)init {
   if (self = [super init]) {
     objects = [[NSMutableDictionary alloc] init];
+    displayCache = [[NSMutableDictionary alloc] init];
+    isLeafCache = [[NSMutableDictionary alloc] init];
+    childCache = [[NSMutableDictionary alloc] init];
     return self;
   }
   return nil;
+}
+
+- (void)clearCache {
+  [displayCache removeAllObjects];
+  [isLeafCache removeAllObjects];
+  [childCache removeAllObjects];
 }
 
 - (id)objectForId:(NSString *)key {
@@ -41,6 +81,7 @@
     closure_f = caml_named_value("fluid_column_browser_children_count");
   }
 
+  // printf("Calling children count\n");
   return Int_val(caml_callback2(*closure_f, Val_int(self.childrenCount), Val_NSString(item_s)));
 }
 
@@ -49,41 +90,63 @@
   CAMLlocal1(tuple_v);
 
   NSString *item_s = (NSString *)item;
+  MyPair *key = [MyPair alloc];
+  key.id = item_s;
+  key.child = index;
+  NSString *obj = [childCache objectForKey:key];
 
-  static value * closure_f = NULL;
-  if (closure_f == NULL) {
-    closure_f = caml_named_value("fluid_column_browser_child_of_item");
+  if (obj == nil) {
+    static value * closure_f = NULL;
+    if (closure_f == NULL) {
+      closure_f = caml_named_value("fluid_column_browser_child_of_item");
+    }
+    tuple_v = caml_alloc_tuple(2);
+    Store_field(tuple_v, 0, Val_NSString(item_s));
+    Store_field(tuple_v, 1, Val_int(index));
+    // printf("Calling childOf\n");
+    NSString *result = NSString_val(caml_callback2(*closure_f, Val_int(self.childOfItem), tuple_v));
+    NSString *child = [self objectForId:result];
+    [childCache setObject:child forKey:key];
+    obj = child;
   }
-  tuple_v = caml_alloc_tuple(2);
-  Store_field(tuple_v, 0, Val_NSString(item_s));
-  Store_field(tuple_v, 1, Val_int(index));
-  NSString *result = NSString_val(caml_callback2(*closure_f, Val_int(self.childOfItem), tuple_v));
-  NSString *child = [self objectForId:result];
-  CAMLreturnT(id, child);
+
+  CAMLreturnT(id, obj);
 }
 
 - (BOOL)browser:(NSBrowser *)browser 
      isLeafItem:(id)item {
   NSString *item_s = (NSString *)item;
+  NSNumber *obj = [isLeafCache objectForKey:item_s];
   
-  static value * closure_f = NULL;
-  if (closure_f == NULL) {
-    closure_f = caml_named_value("fluid_column_browser_is_leaf_item");
+  if (obj == nil) {
+    static value * closure_f = NULL;
+    if (closure_f == NULL) {
+      closure_f = caml_named_value("fluid_column_browser_is_leaf_item");
+    }
+
+    // printf("Calling isLeaf %s\n", [item_s UTF8String]);
+    obj = [NSNumber numberWithBool:Val_true == caml_callback2(*closure_f, Val_int(self.isLeafItem), Val_NSString(item_s))];
+    [isLeafCache setObject:obj forKey:item_s];
   }
 
-  return Val_true == caml_callback2(*closure_f, Val_int(self.isLeafItem), Val_NSString(item_s));
+  return [obj boolValue];
 }
 
-- (id)browser:(NSBrowser *)browser 
-objectValueForItem:(id)item {
+- (id)browser:(NSBrowser *)browser objectValueForItem:(id)item {
   NSString *item_s = (NSString *)item;
-  
-  static value * closure_f = NULL;
-  if (closure_f == NULL) {
-    closure_f = caml_named_value("fluid_column_browser_display_for_item");
+  NSString *obj = [displayCache objectForKey:item_s];
+
+  if (obj == nil) {
+    // printf("Calling display\n");
+    static value * closure_f = NULL;
+    if (closure_f == NULL) {
+      closure_f = caml_named_value("fluid_column_browser_display_for_item");
+    }
+    obj = NSString_val(caml_callback2(*closure_f, Val_int(self.displayForItem), Val_NSString(item_s)));
+    [displayCache setObject:obj forKey:item_s];
   }
 
-  return NSString_val(caml_callback2(*closure_f, Val_int(self.displayForItem), Val_NSString(item_s)));
+  return obj;
 }
 
 @end
@@ -121,6 +184,8 @@ void fluid_column_browser_update(value browser_v, value trackers) {
   delegate.childOfItem = Int_val(Field(trackers, 1));
   delegate.isLeafItem = Int_val(Field(trackers, 2));
   delegate.displayForItem = Int_val(Field(trackers, 3));
+  [delegate clearCache];
+  [browser setNeedsDisplay];
 
   CAMLreturn0;
 }
@@ -138,20 +203,15 @@ CAMLprim value fluid_column_browser_selected_cell(value browser_v) {
   CAMLparam1(browser_v);
   CAMLlocal1(result);
 
-  printf("Getting selected cell\n");
   NSBrowser *browser = (NSBrowser *)Unwrap(browser_v);
-  printf("growser\n");
   if (browser.selectedCell == nil) {
-    printf("none\n");
     result = Atom(0);
   } else {
-    printf("some\n");
     NSIndexPath* selected = browser.selectionIndexPath;
     NSString* item = (NSString *)[browser itemAtIndexPath:selected];
     result = caml_alloc_tuple(1);
     Store_field(result, 0, Val_NSString(item));
   }
-    printf("ok\n");
 
   CAMLreturn(result);
 }
